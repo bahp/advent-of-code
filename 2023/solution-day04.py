484a8bb26b9934083b08a83bc7f64a951cc8f25b
bahp <bernard.hernandez.perez@gmail.com>
2023-12-07 16:13:37 +0000

    ~ day04 minor edit

--- 2023/solution-day04.py
+++ 2023/solution-day04.py
@@ -32,91 +32,125 @@ for i,l in enumerate(lines):
 print(cumu)
 
 
-# ---------------------------
-# Part 2
-# ---------------------------
-# .. note: Extracted from the reddit thread and included
-#          a minor modification. This is a nice and
-#          efficient solution.
 
-cards = []
-for row in lines:
-    game = row.split(":")
-    numbers = game[1].split("|")
-    gid = re.findall(r'\d+', game[0])
-    left = set(re.findall(r'\d+', numbers[0]))
-    right = set(re.findall(r'\d+', numbers[1]))
+def part1(input):
+    """Solution to part 1."""
+    # Get lines
+    lines = input.split("\n")
 
-    win = len(left & right)
+    # Loop
+    cumu = 0
+    for i, l in enumerate(lines):
+        card, numbers = l.split(":")
+        numbers = numbers.split("|")
+        card_id = i
+
+        # Find matching numbers
+        numbers_win = set(re.findall(r"\d+", numbers[0]))
+        numbers_own = set(re.findall(r"\d+", numbers[1]))
+        N = len(numbers_own & numbers_win)
+
+        if N:
+            cumu += 2 ** (N - 1)
+
+    # Return
+    return cumu
+
+
+def part2(input):
+    """Solution to part 2.
+
+    .. note: This solution is really slow because it goes
+             recomputing cards for which we already now
+             the matching. See above for a cleaner solution
     """
-    win = 0
-    for n in left:
-        for k in right:
-            if n == k:
-                win += 1
-    """
+    # Libraries
+    from collections import deque
 
-    cards.append([int(gid[0]), win, 1])
+    def get_card_id(txt):
+        """Extract id number from line"""
+        return int(re.search(r'\d+', txt.split(":")[0]).group(0))
 
+    def get_matches(txt):
+        """Extract set with matching numbers"""
+        aux = txt.split(":")[1]
+        numbers_win = set(re.findall('\d+', aux.split("|")[0]))
+        numbers_own = set(re.findall('\d+', aux.split("|")[1]))
+        return numbers_own.intersection(numbers_win)
 
-for gid, win, copies in cards:
-    for i in range(win):
-        cards[gid+i][2] += copies
+    lines = input.split("\n")
 
-# Count scratchcards
-sums = 0
-for a,b,c in cards:
-    sums += c
+    # Create empty queue
+    q = deque(lines)
+    c = 0
+    m = {}
 
-# Answer
-print(sums)
+    # Loop
+    while len(q):
+        c += 1
 
+        item = q.popleft()
+        card_id = get_card_id(item)
 
-import sys
-sys.exit()
+        if not card_id in m:
+            m[card_id] = get_matches(item)
 
+        for i in range(len(m[card_id])):
+            q.append(lines[card_id + i])
 
-
-# ---------------------------
-# Part 2
-# ---------------------------
-# .. note: This solution is really slow because it goes
-#          recomputing cards for which we already now
-#          the matching. See above for a cleaner solution
-
-# Libraries
-from collections import deque
-
-def get_card_id(txt):
-    """Extract id number from line"""
-    return int(re.search(r'\d+', txt.split(":")[0]).group(0))
-
-def get_matches(txt):
-    """Extract set with matching numbers"""
-    aux = txt.split(":")[1]
-    numbers_win = re_number_list(aux.split("|")[0])
-    numbers_own = re_number_list(aux.split("|")[1])
-    return numbers_own.intersection(numbers_win)
+    # Answer
+    return c
 
 
-# Create empty queue
-q = deque(lines)
-c = 0
+def part2_reddit(input):
+    """Solution to part 2 from reddit."""
+    lines = input.split("\n")
+    cards = []
+    for row in lines:
+        game = row.split(":")
+        numbers = game[1].split("|")
+        gid = re.findall(r'\d+', game[0])
+        left = set(re.findall(r'\d+', numbers[0]))
+        right = set(re.findall(r'\d+', numbers[1]))
 
-# Loop
-while len(q):
-    c += 1
+        win = len(left & right)
 
-    item = q.popleft()
-    card_id = get_card_id(item)
-    matches = get_matches(item)
+        """
+        .. note: This was in the original solution.
+        
+        win = 0
+        for n in left:
+            for k in right:
+                if n == k:
+                    win += 1
+        """
 
-    for i in range(len(matches)):
-        q.append(lines[card_id+i])
+        cards.append([int(gid[0]), win, 1])
 
-# Answer
-print(c)
+    for gid, win, copies in cards:
+        for i in range(win):
+            cards[gid + i][2] += copies
+
+    # Count scratchcards
+    sums = 0
+    for a, b, c in cards:
+        sums += c
+
+    # return
+    return sums
 
 
 
+# --------------------------
+# Main
+# --------------------------
+# path
+path = Path('data/day04')
 
+with open(path / 'sample02.txt', 'r') as f:
+    lines  = f.read()
+
+# Show
+print(part1(lines))
+print(part2(lines))
+print(part2_reddit(lines))
