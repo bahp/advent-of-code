71cd577bf6af68ea41e0cdc82be0c8ef82307721
bahp <bernard.hernandez.perez@gmail.com>
2023-12-08 16:42:49 +0000

    ~ aoc 2022 day05

--- 2022/solution-day05.py
+++ 2022/solution-day05.py
@@ -4,49 +4,85 @@ import numpy as np
 from pathlib import Path
 from collections import deque
 
+
+def parse_moves(input):
+    """Parse instructions."""
+    moves = input.split("\n\n")[1]
+    return [
+        tuple(map(int, re.findall(r"\d+", move)))
+            for move in moves.strip().split("\n")
+    ]
+
+
+def parse_stacks(input):
+    """Parse the stacks.
+
+    .. note: Assumes stacks only have one letter components and
+             exploits the fact that they are a fixe distance
+             apart from each other.
+    """
+    board, moves = input.split("\n\n")
+    numbers = re.findall('\d+', board.split("\n")[-1])
+    stacks = [""] * max(list(map(int, numbers)))
+    for line in board.split("\n")[:-1]:
+        for i, box in enumerate(line[1::4]):
+            if box != " ":
+                stacks[i] += box
+    return stacks
+
+
+def solvecrane(input, is_cr9001=False):
+    """Solve crane problem.
+
+    Params
+    ------
+    is_cr9001: bool
+       If true, various crates are moved at once and therefore
+       the order remains. Otherwise, crates can only be moved
+       one by one.
+    """
+    # Get information
+    stacks = parse_stacks(input)
+    moves = parse_moves(input)
+
+    # Loop
+    for m in moves:
+        n, f, t = m
+        mv = stacks[f - 1][:n]
+        mv = mv if is_cr9001 else mv[::-1]
+        stacks[t - 1] = mv + stacks[t - 1]
+        stacks[f - 1] = stacks[f - 1][n:]
+
+    # Return
+    return stacks
+
+
+def part1(input):
+    """Solution part 1."""
+    stacks = solvecrane(input, is_cr9001=False)
+    answer = ''.join([e[0] for e in stacks])
+    return answer
+
+
+def part2(input):
+    """Solution part 2."""
+    stacks = solvecrane(input, is_cr9001=True)
+    answer = ''.join([e[0] for e in stacks])
+    return answer
+
+
+
+
+
+# ---------------------------------------------
+# Main
+# ---------------------------------------------
 # Path
 path = Path('data/day05')
 
-with open(path / 'sample01.txt', 'r') as f:
+with open(path / 'sample02.txt', 'r') as f:
     lines = f.read()
 
-print(lines)
-
-n = 4
-
-# Divide into crates and moves
-crates, moves = lines.split("\n\n")
-crates = crates.split("\n")
-moves = moves.split("\n")
-
-board = np.array([list(l) for l in crates])
-
-print(board)
-import sys
-sys.exit()
-
-# Build stacks
-stacks = []
-for l in crates[:-1]:
-    # Get chunks of 4 characters
-    aux = [l[i:i+n][1] for i in range(0, len(l), n)]
-
-    # Create stack
-    stack = deque([])
-    for e in aux:
-        if e.isalpha():
-            stack.append(e)
-    stacks.append(stack)
-
-# Perform moves
-for m in moves:
-    q, f, t = re.findall('\d+', m)
-    print(stacks)
-    print(q, f, t)
-    for i in range(int(q)):
-        e = stacks[int(q)-1].pop()
-        stacks[int(t)-1].append(e)
-
-
-
-print(stacks)
+# Show
+print(part1(lines))
+print(part2(lines))
