c7505231d5dd97a2fcd0c786c580329422e83106
Imperial College <cbit@dyn3142-184.wlan.ic.ac.uk>
2023-12-12 10:29:47 +0000

    aof solution 2023 day11

---
+++ 2023/solution-day11.py
@@ -0,0 +1,140 @@
+import re
+import math
+import numpy as np
+import sys
+import time
+
+from pathlib import Path
+from functools import wraps
+from collections import Counter
+from itertools import combinations
+
+# This is the adjacency list uset to compute the neighbors. It
+# It contains for the current location, the possible movements
+# towards North (N), East (E), South (S) and West (W) of the
+# map.
+# N = -1, S = 1, W = -1, E = 1
+ADJ = {
+    '.': [],  # No pipe
+    '|': [[-1, 0], [1, 0]],  # N-S
+    '-': [[0, 1], [0, -1]],  # E-W
+    'L': [[-1, 0], [0, 1]],  # N-E
+    'J': [[-1, 0], [0, -1]], # N-W
+    '7': [[1, 0], [0, -1]],  # S-W
+    'F': [[1, 0], [0, 1]],   # S-E
+    'S': [[1,0], [0,1], [-1,0], [0, -1]],  # N-S-W-E
+}
+
+
+def numpy2str(m):
+    return '\n'.join([''.join(e) for e in m])
+
+
+def parse(input):
+    return np.array([np.array(list(l))
+        for l in input.splitlines()])
+
+
+def expand_map(m):
+    """Expand the map with galaxies"""
+    # Find vectors to repeat
+    r, c = m.shape
+    cols = (np.sum(m=='.', axis=0) == c) + 1
+    rows = (np.sum(m=='.', axis=1) == r) + 1
+
+    # Repeat
+    E = np.repeat(m, cols, axis=1)
+    E = np.repeat(E, rows, axis=0)
+    
+    # Return
+    return E
+
+def expand_coords(coords, m, coef=1e6):
+    """Expand just the coordinates of galaxies"""
+    r, c = M.shape
+    cols = np.argwhere(np.sum(m == '.', axis=0) == c).flatten()
+    rows = np.argwhere(np.sum(m == '.', axis=1) == r).flatten()
+
+    expanded = []
+    for i, j in coords:
+        a = i + (coef-1)*(np.sum(i>=rows))
+        b = j + (coef-1)*(np.sum(j>=cols))
+        expanded.append((a,b))
+
+    return expanded
+
+
+def manhattan(a, b):
+    """Compute the manhattan distance"""
+    return sum(abs(val1-val2) for val1, val2 in zip(a,b))
+
+
+def part1_map():
+    """Solution part 1.
+
+    .. note: We are manually expending the rows and columns. And
+             computing the manhattan distance for each pair of
+             galaxies. This approach is very limited, not feasible
+             to expand each row/col by a factor of 1e6 (part2).
+    """
+    # Find galaxies on the expanded map
+    galaxies = np.argwhere(expand_map(M) == '#').tolist()
+    return int(np.sum([manhattan(c0, c1)
+        for c0, c1 in combinations(galaxies, 2)
+    ]))
+
+
+def part1():
+    """Solution part 1"""
+    galaxies = np.argwhere(M == '#').tolist()
+    galaxies = expand_coords(galaxies, M, coef=2)
+    return int(np.sum([manhattan(c0, c1)
+        for c0, c1 in combinations(galaxies, 2)
+    ]))
+
+
+def part2():
+    """Solution part 2"""
+    galaxies = np.argwhere(M == '#').tolist()
+    galaxies = expand_coords(galaxies, M, coef=1e6)
+    return int(np.sum([manhattan(c0, c1)
+        for c0, c1 in combinations(galaxies, 2)
+    ]))
+
+
+def part2_4hbQ(filename):
+    """Solution from 4hbQ.
+
+    .. note: This is golfing... trying to make code short.
+    """
+    return sum(map(lambda g:
+        sum(sum(map(i.__ge__, g)) * (i in g or 1000000) *
+            sum(map(i.__lt__, g)) for i in range(max(g))),
+                zip(*[(x, y) for y, r in enumerate(open(filename))
+                    for x, c in enumerate(r) if c == '#'])))
+
+
+
+
+# -------------------------------------
+# Main
+# -------------------------------------
+# .. note: If issue the recursive approach, it is necessary
+# to increase the recursion limit to compute the solution.
+# Otherwise it will raise a recursion limit exceeded error.
+#sys.setrecursionlimit(1000000)
+
+# Path
+path = Path('data/day11') / 'sample01.txt'
+
+with open(path) as f:
+    lines  = f.read()
+
+# Define the map globally
+M = parse(lines)
+
+# Show
+print("Part1 (map):    %s" % part1_map())
+print("Part1 (coords): %s" % part1())
+print("Part2 (coords): %s" % part2())
+print("Part2 (4hbQ):   %s" % part2_4hbQ(path))
